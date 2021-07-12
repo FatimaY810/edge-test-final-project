dbb6799afd7930bf26fd4b9f1990f9ba6740c604
FatimaY810 <fatimayousaf810@gmail.com>
2021-07-12 11:31:56 +0300

    Update Edge Final Project.py

--- Edge Final Project.py
+++ Edge Final Project.py
@@ -1,9 +1,27 @@
 #A quiz designed to help sort you into your generation, based on your answers to the following questions:
-#Question Layout:
-# Questtion 1: What Social Media App do you use most often?
-# a. Tiktok
-# b. Instagram
-# c. Facebook
-# d. I prefer face to face interactions
-Question 2: What was your first telecommunication device (trying to not make the oldies feel older :) )
-a. Cellphone
+Total_Score = 0
+print ("Ever wondered if you were born in the right generation? Well today's your lucky day... LET'S PLAY GENERATION Q(uiz)!")
+command = input("Type START to start, EXIT to end game, HELP for instructions: ")
+while command != "EXIT":
+    command = input("Question 1: What Social Media App do you use most often? \n a. TikTok \n b. Instagram \n c. Facebook \n d. I prefer face-to-face commnication \n ANS HERE: ")
+    if command == "a":
+        print ("good answer! On your way to becoming Tiktok Famous :p")
+        Total_Score = Total_Score + 10
+    elif command == "b":
+        print ("Nice! Getting the likes and followers :)")
+        Total_Score = Total_Score + 20
+    elif command == "c":
+        print ("Cool! How are the embarassing family pictures going? :)")
+        Total_Score = Total_Score + 30
+    elif command == "d":
+        print ("old school is underrated!")
+        Total_Score = Total_Score + 40
+    else:
+        print ("ERROR")
+    command_2 = input ("Question 2: What was your first telecommunication device (trying to not make the oldies feel older :) )")
+
+
+
+
+
+print ("end of game")
