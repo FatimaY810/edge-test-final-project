8f11caf7f8f429de51cfc2b4ca079d10b9e2b75d
FatimaY810 <fatimayousaf810@gmail.com>
2021-07-12 10:42:11 +0300

    Create Edge Final Project.py

---
+++ Edge Final Project.py
@@ -0,0 +1,8 @@
+#A quiz designed to help sort you into your generation, based on your answers to the following questions:
+#Question Layout:
+# Questtion 1: What Social Media App do you use most often?
+# a. Tiktok
+# b. Instagram
+# c. Facebook
+# d. I prefer face to face interactions
+Question 2:
